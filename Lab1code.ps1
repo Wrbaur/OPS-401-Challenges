30f2abc11d81e8c2e6b32a489245cc390c294c2d
Will <wrbaur@gmail.com>
2024-01-08 17:40:06 -0600

    Lab1code.ps1

---
+++ Lab1code.ps1
@@ -0,0 +1,17 @@
+# Script: Ops 401 Lab 1
+# Authur: Will B. with the help of ChatGPT
+# Date: 1/08/2024
+
+# PowerShell cript to adjust auto screen lock
+
+# Set Screen Lock Timeout (in seconds)
+$timeoutInSeconds = 150
+
+# Configure Automatic Screen Lock
+powercfg /change standby-timeout-dc $timeoutInSeconds
+powercfg /change standby-timeout-ac $timeoutInSeconds
+powercfg /change monitor-timeout-dc $timeoutInSeconds
+powercfg /change monitor-timeout-ac $timeoutInSeconds
+
+# Display Configuration Status
+powercfg /query SCHEME_CURRENT SUB_SLEEP STANDBYIDLE
